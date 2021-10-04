284e871c8bcfdc31348371de31b6b088c40b668e
dmartinez91 <dmartinez3691@gmail.com>
2021-10-04 19:19:20 -0400

    deleted files that were not necessary and refactored Show user bets by taking out code

--- src/components/Userbets/ShowUserbets.jsx
+++ src/components/Userbets/ShowUserbets.jsx
@@ -1,58 +1,47 @@
 import axios from "axios";
-import React, {useEffect, useState} from 'react';
-import jwtDecode from "jwt-decode";
+import React, { useEffect, useState } from "react";
 import Userbets from "./Userbets";
 import CreateBet from "./CreateBet";
-import LoginUser from "../Login/LoginHooks";
-
-
 
 const ShowUserbets = () => {
-    const [searchResults, setSearchResults] = useState([]);
-    const [newBet, setNewBet] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [newBet, setNewBet] = useState([]);
 
-    useEffect(() => {
-        makeGetRequest();
-        createBetSlip();
-    }, [])
+  useEffect(() => {
+    makeGetRequest();
+    createBetSlip();
+  }, []);
 
-    const makeGetRequest = async () => {
-        try{
-            let response = await axios.get('http://127.0.0.1:8000/api/bets/all/')
-            console.log(response)
-            if (response.data) {
-                setSearchResults(response.data)
-                console.log('below is the shit')
-               
-            }
-            
-        }
-        catch(ex){
-            console.log(ex)
-        }
+  const makeGetRequest = async () => {
+    try {
+      let response = await axios.get("http://127.0.0.1:8000/api/bets/all/");
+      console.log(response);
+      if (response.data) {
+        setSearchResults(response.data);
+        console.log("below is the shit");
+      }
+    } catch (ex) {
+      console.log(ex);
     }
-    
-    const createBetSlip = async () => {
-        try{
-            let response = await axios.post('http://127.0.0.1:8000/api/bets/')
-            if (response.data){
-                setNewBet(response.data)
-            }
-        }
-        catch(ex){
-            console.log(ex)
-        }
+  };
+
+  const createBetSlip = async () => {
+    try {
+      let response = await axios.post("http://127.0.0.1:8000/api/bets/");
+      if (response.data) {
+        setNewBet(response.data);
+      }
+    } catch (ex) {
+      console.log(ex);
     }
+  };
 
-    return (
-        <div> 
-            
-            <Userbets displaySearch={searchResults}/>
-            <CreateBet makeBet={createBetSlip}/>
-            
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <Userbets displaySearch={searchResults} />
+      <CreateBet makeBet={createBetSlip} />
+    </div>
+  );
+};
 
 export default ShowUserbets;
