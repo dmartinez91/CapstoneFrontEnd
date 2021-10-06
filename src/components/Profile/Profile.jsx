0366b4dc29fb0138d910aab89ac805e4f8abd69c
dmartinez91 <dmartinez3691@gmail.com>
2021-10-06 03:35:58 -0400

    refactored profile to show winnings and losses of bet slips along with total amount of money won based on over all money lost and won

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-import LoginUser from "../Login/LoginHooks";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import currentUser from "../App";
 
 const Profile = () => {
   const user = currentUser();
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
+  const [moneyWon, setMoneyWon] = useState(0);
+  const [moneyLost, setMoneyLost] = useState(0);
+  let winningPercentage = parseFloat(wins / (losses + wins)).toFixed(3);
+  let grossWinnings = moneyWon - moneyLost;
 
   return (
     <div className="container">
@@ -13,20 +19,56 @@ const Profile = () => {
         </h3>
       </header>
       <p>
-        <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
-        {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-      </p>
-      <p>
         <strong>Id:</strong> {currentUser.id}
       </p>
       <p>
         <strong>Email:</strong> {currentUser.email}
       </p>
-      <strong>Authorities:</strong>
-      <ul>
-        {currentUser.roles &&
-          currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-      </ul>
+      <p>
+        <strong>Total Winnings ${grossWinnings} </strong>
+      </p>
+      <p>
+        <strong>
+          Betting Record: {wins} - {losses} Percentage: {winningPercentage} %
+        </strong>
+      </p>
+      <React.Fragment>
+        <div>
+          <Button
+            onClick={() => {
+              setWins((c) => c + 1);
+            }}
+          >
+            {" "}
+            +
+          </Button>
+          Wins: {wins}
+          <Button
+            onClick={() => {
+              setLosses((c) => c + 1);
+            }}
+          >
+            +
+          </Button>
+          Losses: {losses}
+        </div>
+        <label>
+          Money Won:
+          <input
+            type="text"
+            value={moneyWon}
+            onChange={(event) => setMoneyWon(event.target.value)}
+          />
+        </label>
+        <label>
+          Money Lost:
+          <input
+            type="text"
+            value={moneyLost}
+            onChange={(event) => setMoneyLost(event.target.value)}
+          />
+        </label>
+      </React.Fragment>
     </div>
   );
 };
